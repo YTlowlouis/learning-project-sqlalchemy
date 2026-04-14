4fb068c8350775ce53f9d64032f5c9014c66a574
YTlowlouis <mottemotte350@gmail.com>
2026-04-14 23:14:45 +0200

    fjoiep

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Mapped
 from typing import List
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import select
 
 engine = create_engine("sqlite:///database.db", echo=False)
 
@@ -18,16 +19,16 @@ class User(Base):
     name: Mapped[str] = mapped_column(String(10), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
 
-    #posts: Mapped[List["Post"]] = relationship(back_populates="author")
+    posts: = relationship("Post", back_populates="author")
 
-    #liked_post: Mapped[List["Post"]] = relationship("Post", se)
 
 class Post(Base):
     __tablename__ = "post"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
 
-    #author = relationship("User", back_populates="name")
+    user_id = mapped_column(ForeignKey("user.id"))
+    author = relationship("User", back_populates="posts")
 
 def init_db():
     Base.metadata.create_all(engine)
@@ -38,11 +39,14 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 user = User(name="alice", email="alice@test.com")
+user2 = User(name="bob", email="bob@test.com")
 
 session.add(user)
+session.add(user2)
+
+
 session.commit()
 
-users = session.query(User).all()
-
-for u in users:
-    print(u.name)
+users = select(User.name).where(User.name.in_(["alice", "bob"]))
+for user in session.scalars(users):
+    print(user)
